10c7118244fc271a036a9cd181d66906bb97ae1a
Raslif.A <raslif1234@gmail.com>
2022-03-18 17:36:42 +0400

    Add Entity Framework Core and Migrations of the Identity Tables

---
+++ IdentityManager/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityManager.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions dbContextOptions)
+            : base(dbContextOptions)
+        {
+
+        }
+    }
+}
